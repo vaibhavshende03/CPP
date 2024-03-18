83bbd7f9f9515eb246e6898a1e6a3bca0f61e55f
vaibhavshende03 <jitcsevaibhavshende@gmail.com>
2024-03-18 21:33:44 +0530

    Update

--- cpp/patterns/pattern_12.cpp
+++ cpp/patterns/pattern_12.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 using namespace std;
+/*
+1
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
 
+*/
 int main()
 {
 
     int n{};
     cout << "Enter The Number :";
     cin >> n;
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= i)
+    //     {
+    //         cout << i - j + 1 << " ";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i - j + 1 << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
--- cpp/patterns/pattern_13.cpp
+++ cpp/patterns/pattern_13.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 using namespace std;
-
+/*
+5
+5 4
+5 4 3
+5 4 3 2
+5 4 3 2 1
+*/
 int main()
 {
 
     int n{};
     cout << "Enter The Number :";
     cin >> n;
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= i)
+    //     {
+    //         cout << n - j + 1 << " ";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << n - j + 1 << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
