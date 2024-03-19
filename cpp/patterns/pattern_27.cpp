13a16e47976af2cb940c9b1900f0261489c81d72
vaibhavshende03 <jitcsevaibhavshende@gmail.com>
2024-03-20 01:47:52 +0530

    Update

---
+++ cpp/patterns/pattern_27.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+/*
+1234554321
+1234**4321
+123****321
+12******21
+1********1
+*/
+int main()
+{
+
+    int n{};
+
+    cout << "Enter The number :";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        // triangle1
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << j;
+        }
+
+        // triangle2
+        for (int k = 3; k <= i * 2; k++)
+        {
+            cout << "*";
+        }
+
+        // triangle3
+
+        for (int l = n - i + 1; l >= 1; l--)
+        {
+            cout << l;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
