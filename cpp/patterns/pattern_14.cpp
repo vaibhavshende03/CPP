2c1a20f3a63f601de0330a2984edbf486a7ca95c
vaibhavshende03 <jitcsevaibhavshende@gmail.com>
2024-03-19 17:14:52 +0530

    Update

--- cpp/patterns/pattern_14.cpp
+++ cpp/patterns/pattern_14.cpp
@@ -1,3 +1,14 @@
+/*
+pattern :
+
+A A A A A A
+B B B B B B
+C C C C C C
+D D D D D D
+E E E E E E
+F F F F F F
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -5,8 +16,34 @@ int main()
 {
 
     int n{};
-    cout << "Enter The Number :";
+
+    cout << "Enter The number :";
     cin >> n;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= n; ++j)
+        {
+            char ch = 'A' + i - 1;
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+
+    //--------------------------------------------------------------------------
+    // char n{};
+
+    // cout << "Enter The number :";
+    // cin >> n;
+
+    // for (char i = 'A'; i <= n; ++i)
+    // {
+    //     for (char j = 'A'; j <= n; ++j)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
 
     return 0;
 }
---
+++ cpp/patterns/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+ cout << "Enter The number :";
+    cin >> n;
